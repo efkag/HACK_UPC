b40e496df8badb881e1740b30c5d4a016bc9524c
efkag <kagioulis.efstathios@gmail.com>
2019-10-12 15:44:44 +0200

    test conv net and add function to creat traingin sample

--- model/convNet.py
+++ model/convNet.py
@@ -5,19 +5,18 @@ tfkb = tfk.backend
 tfkl = tfk.layers
 
 
-
-class ConvNet:  # Convolutional
+class ConvNet:  # Convolutional Net
     def __init__(self, input_shape=(), epochs=100, learning_rate=0.001, batch_size=50, padding='same',
                  kernel_size=None, stride=2):
         if kernel_size is None:
-            self.kernel_size = [5, 5]
+            self.kernel_size = [3, 3]
         else:
             self.kernel_size = kernel_size
-        self.epochs=epochs
+        self.epochs = epochs
         self.input_shape = input_shape
+        print(self.input_shape)
         self.stride = stride
         self.filters = 4  # The base filter number
-        self.filter_mult = 2
         self.out_activ_func = 'sigmoid'
         self.activ_func = 'relu'
         self.padding = padding  # SAME or VALID
@@ -26,7 +25,8 @@ class ConvNet:  # Convolutional
 
         self.model = None
         # Network structure
-        self.images = tfk.Input(dtype=tf.float32, shape=input_shape + (1,), name="Images")
+        self.x = None
+        #self.images = tfk.Input(dtype=tf.float32, shape=input_shape, name="Images")
         self.structure_net()
 
         optimizer = tf.keras.optimizers.Adam(learning_rate)
@@ -39,12 +39,15 @@ class ConvNet:  # Convolutional
     def structure_net(self):
 
         self.model = tfk.models.Sequential([
-            tfkl.Conv2D(self.filters, self.kernel_size, self.stride,  padding='same', activation=self.activ_func, input_shape=self.input_shape + (1,)),
-            tfkl.MaxPooling2D(),
-            tfkl.Conv2D(self.filters * 2, self.kernel_size, padding=self.padding, activation=self.activ_func),
-            tfkl.MaxPooling2D(),
-            tfkl.Conv2D(self.filters * 4, self.kernel_size, padding=self.padding, activation=self.activ_func),
-            tfkl.MaxPooling2D(),
+            tfkl.Conv2D(self.filters, self.kernel_size, self.stride,  padding='same',
+                        activation=self.activ_func, input_shape=self.input_shape, data_format='channels_last'),
+            tfkl.MaxPool2D((2, 2), data_format='channels_last'),
+            tfkl.Conv2D(self.filters * 2, self.kernel_size, padding=self.padding, activation=self.activ_func,
+                        data_format='channels_last'),
+            tfkl.MaxPool2D((2, 2), data_format='channels_last'),
+            tfkl.Conv2D(self.filters * 4, self.kernel_size, padding=self.padding, activation=self.activ_func,
+                        data_format='channels_last'),
+            tfkl.MaxPool2D((2, 2)),
             tfkl.Flatten(),
             tfkl.Dense(512, activation=self.activ_func),
             tfkl.Dense(1, activation=self.activ_func)
