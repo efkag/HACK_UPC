6198e3f54d6104e84ae68d9a405dd812b55cec70
efkag <kagioulis.efstathios@gmail.com>
2019-10-12 10:38:06 +0200

    add display function

--- model/utils.py
+++ model/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 path = 'Dataset/training_data/'
 
@@ -11,8 +12,24 @@ def load_data(min, max):
     return imgs
 
 
+def display_image(image):
+    """
+    Display the image given as a 2d or 3d array of values.
+    :param image: Input image to display
+    """
+    image = np.squeeze(image)
+    plt.imshow(image, interpolation='bilinear')
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    # or plt.axis('off')
+    plt.show()
+
+
+def sample_from_each_class(data):
+    sample_data = []
+    sample_data.append(np.random.choice(data))
+
+
+
+# Example to load and display images
 images = load_data(0, 5)
-
-cv.imshow('image', images[0])
-
-
+display_image(images[0])
