48923e9ce7f88a8ba7078fdc79c30b01befed742
efkag <kagioulis.efstathios@gmail.com>
2019-10-12 11:11:26 +0200

    Add function to sample from the dataset equal samples from each class. Add function to load data by given indexes list

--- model/utils.py
+++ model/utils.py
@@ -6,7 +6,7 @@ imgs_path = 'Dataset/training_data/'
 labels_path = 'Dataset/training_labels.txt'
 
 
-def load_data(min, max):
+def load_data_range(min, max):
     """
     Load images from the dataset within a given range
     :param min: Lower bound for array index
@@ -15,6 +15,19 @@ def load_data(min, max):
     """
     imgs = []
     for img_file in range(min, max):
+        imgs.append(cv.imread(imgs_path + str(img_file) + '.jpg'))
+
+    return imgs
+
+
+def load_data_indexes(indexes):
+    """
+    Load image that are in the given index list
+    :param indexes: List of indexes
+    :return: List of images
+    """
+    imgs = []
+    for img_file in indexes:
         imgs.append(cv.imread(imgs_path + str(img_file) + '.jpg'))
 
     return imgs
@@ -45,15 +58,38 @@ def load_labels():
     return labels
 
 
-def sample_from_each_class(data):
-    sample_data = []
-    sample_data.append(np.random.choice(data))
+def sample_rand_from_each_class(samples_num):
+    """
+    Sample a gicven number of images from each class
+    :param samples_num: Samples per class
+    :return: Full dataset sample (should be of size samples_num * 101)
+    """
+    data_pointer = 0
+    interval = 1001
+    dataset_sample = []
+
+    for i in range(0, 101):
+        class_sample = []
+        # Sample indexes with the class range
+        indexes = np.random.randint(data_pointer, data_pointer + interval, samples_num)
+        # Load all images given by the indexes
+        class_sample = load_data_indexes(indexes)
+        # Append to the dataset sample
+        dataset_sample.extend(class_sample)
+
+    print(len(dataset_sample))
+    return dataset_sample
+    # sample_data = []
+    # sample_data.append(np.random.choice(data))
 
 
-# Example to load and display images
-images = load_data(0, 5)
-display_image(images[0])
+# # Example to load and display images
+# images = load_data(0, 5)
+# display_image(images[0])
+#
+# # Example to load labels
+# labels = load_labels()
+# print(len(labels))
 
-# Example to load labels
-labels = load_labels()
-print(len(labels))
+#Sample from all classes
+sample_rand_from_each_class(20)
